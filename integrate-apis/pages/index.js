f2d80b7e3cc4ac29c13f00896e879ec8c1a24b9d
youdame <112458620+youdame@users.noreply.github.com>
2023-11-28 16:34:01 +0900

    Feat : 파일 재구성

--- integrate-apis/pages/index.js
+++ integrate-apis/pages/index.js
@@ -1,23 +1,23 @@
+import { useEffect, useState } from "react";
 import MovieList from "@/components/MovieList";
 import SearchForm from "@/components/SearchForm";
 import styles from "@/styles/Home.module.css";
 import Header from "@/components/Header";
 import Container from "@/components/Container";
+import axios from "@/lib/axios";
 
-import { useEffect, useState } from "react";
-import axios from '@/lib/axios';
 export default function Home() {
   const [movies, setMovies] = useState([]);
 
   async function getMovies() {
-    const result = await axios.get(`/movies`);
-    console.log(result)
-    const movies = result.data.results ?? [];
-
+    const res = await axios.get("/movies/");
+    const movies = res.data.results ?? [];
     setMovies(movies);
   }
 
-  useEffect(() => {getMovies()}, []);
+  useEffect(() => {
+    getMovies();
+  }, []);
 
   return (
     <>
