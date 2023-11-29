057c0fd63891f3497cb2ecce1e0fc0ce5539f0f1
youdame <112458620+youdame@users.noreply.github.com>
2023-11-29 19:00:45 +0900

    Feat : Image 컴포넌트 사용

--- codeitmall/components/ProductList.js
+++ codeitmall/components/ProductList.js
@@ -1,21 +1,38 @@
+import Image from "next/image";
 import Link from "next/link";
-import styles from "@/styles/ProductList.module.css";
+import styles from "./ProductList.module.css";
+import StarRating from "./StarRating";
 
-export default function ProductList({ products = [] }) {
+export default function ProductList({ className = "", products }) {
   return (
-    <ul className={styles.productList}>
-      {products.map((product) => (
+    <ul className={`${styles.productList} ${className}`}>
+      {products?.map((product) => (
         <li key={product.id}>
           <Link className={styles.product} href={`/items/${product.id}`}>
-            <img
-              src={product.imgUrl}
-              width="300"
-              height="300"
-              alt={product.name}
-            />
-            <span className={styles.productName}>{product.name}</span>
-            <br />
-            {product.price}원
+            <div className={styles.image}>
+              <Image fill src={product.imgUrl} alt={product.name} />
+            </div>
+            <div className={styles.content}>
+              <div>
+                <span className={styles.name}>{product.name}</span>
+                <div className={styles.prices}>
+                  <span className={styles.originalPrice}>
+                    {product.price.toLocaleString()}원
+                  </span>
+                  {product.salePrice.toLocaleString()}원
+                </div>
+              </div>
+              <hr className={styles.divider} />
+              <div>
+                <div className={styles.starRating}>
+                  <StarRating value={product.starRating} />
+                  {product.starRatingCount.toLocaleString()}
+                </div>
+                <div className={styles.likeCount}>
+                  ♥{product.likeCount.toLocaleString()}
+                </div>
+              </div>
+            </div>
           </Link>
         </li>
       ))}
